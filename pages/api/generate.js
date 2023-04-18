2ce8c015ae6ad72e388ea4ff89cdcdac34ab9a05
SurjaHead <95391476+SurjaHead@users.noreply.github.com>
2023-04-18 17:35:30 -0400

    latest

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -44,27 +44,7 @@ const generateAction = async (req, res) => {
 
 	const completion = await openai.createChatCompletion({
 		model: "gpt-3.5-turbo",
-		messages: [{role: "user", content: `'I want you to act as a personal trainer. I will provide you with all the information needed about an individual looking to become fitter, stronger and healthier through physical training, and your role is to devise the best workout and meal plans for that person depending on their current fitness level, goals and lifestyle habits.  I will provide you with the individual's height, weight, age, activity level, availability, access to equipment, diet restrictions, disliked foods, and goals. With this information, you should create an optimized meal plan and workout plan based on your knowledge as a personal trainer. For the meal plan, calculate the individual's necessary calories per day to achieve their goal, using the information given, and ensure the meal plan matches those calories, as well as macros (protein, fat, and carbs). Provide the exact measurements of each ingredient in each meal and show the calories + macronutrients in each meal.
-		Make sure the individual gets 30 grams of fiber everyday with their meal plan. Provide a different meal plan for all 7 days of the week.
-		
-		Provide the output in the following format:
-		Monday Meal Plan:
-		Wednesday Meal Plan:
-		Thursday Meal Plan:
-		Friday Meal Plan:
-		Saturday Meal Plan:
-		Sunday Meal Plan:
-		
-		Monday Workout:
-		Tuesday Workout:
-		Wednesday Workout:
-		Thursday Workout:
-		Friday Workout:
-		Saturday Workout:
-		Sunday Workout:
-		
-		
-		Here's the individual's info:'${req.body.userInput}\n`}],
+		messages: [{role: "user", content: `Hello!`}],
 	});
 	console.log(completion.data.choices[0].message);
 
