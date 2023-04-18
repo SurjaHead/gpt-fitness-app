e585577dbe3eecc8e58078b33c3dcb6863925cd3
SurjaHead <95391476+SurjaHead@users.noreply.github.com>
2023-04-18 16:56:09 -0400

    latest

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -2,6 +2,7 @@ import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
 	apiKey: process.env.OPENAI_API_KEY,
+	organization: "org-4LOysOMzz5TtvgJn2oBEE9DS",
 });
 
 const openai = new OpenAIApi(configuration);
@@ -44,10 +45,9 @@ const generateAction = async (req, res) => {
 	const completion = await openai.createChatCompletion({
 		model: "gpt-3.5-turbo",
 		messages: [{role: "user", content: `${basePromptPrefix}${req.body.userInput}\n`}],
-	  });
-	  
-
-	const basePromptOutput = completion.data.choices.pop();
+	});
+	console.log(completion.data.choices[0].message);
+	const basePromptOutput = baseCompletion.data.choices.pop();
 
 	res.status(200).json({ output: basePromptOutput });
 };
