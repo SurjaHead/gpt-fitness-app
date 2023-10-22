a4848ea98f080b152e866e7197e3cb22af4e0677
Surya Sure <suryasure734@gmail.com>
2023-10-21 20:41:59 -0400

    latest

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -9,60 +9,31 @@
 // const openai = new OpenAIApi(configuration);
 
 const basePromptPrefix = `
-I want you to act as a personal trainer. I will provide you with all the information needed about an individual looking to become fitter, stronger, and healthier through physical training, and your role is to devise the best meal plan for that person depending on their current fitness level, goals, and lifestyle habits.  I will provide you with the individual's height, weight, age, gender, activity level,  preferred foods, diet restrictions, disliked foods, and goals. With this information, you should create an optimized meal plan based on your knowledge as a personal trainer. For the meal plan, calculate the individual's necessary calories per day to achieve their goal, using the information given, and ensure the meal plan matches those calories, as well as macros (protein, fat, and carbs). Provide the exact measurements of each ingredient in each meal and show the calories + macronutrients in each meal. 
+I want you to act as a personal trainer. I will provide you with all the information needed about an individual looking to become fitter, stronger, and healthier through physical training, and your role is to devise the best workout plan for that person depending on their current fitness level, goals, and lifestyle habits.  
 
-Make sure the individual gets 30 grams of fiber every day with their meal plan. Provide a different meal plan for all 7 days of the week that includes the total calorie, fiber, and macronutrient counts for each meal. Once you create the meal plan, double-check to make sure the daily macronutrient and calorie totals equal the required macronutrients for the user. If you stop in the middle, continue where you left off. 
-
-Provide the output in the following format:
-
-Monday Meal Plan:
-Wednesday Meal Plan:
-Thursday Meal Plan:
-Friday Meal Plan:
-Saturday Meal Plan:
-Sunday Meal Plan:
+I will provide you with the individual's height, weight, availability, preferred form of exercise, and access to equipment, and your job is to create the optimal workout plan for that person to accomplish their goal. 
 
 `;
 
-// const generateAction = async (req, res) => {
-// 	console.log(`API: ${basePromptPrefix}${req.body.userInput}`);
+import { Configuration, OpenAIApi } from "openai";
 
-// 	// const baseCompletion = await openai.createCompletion({
-// 	// 	model: "text-davinci-003",
-// 	// 	prompt: `${basePromptPrefix}${req.body.userInput}\n`,
-// 	// 	temperature: 0,
-// 	// 	max_tokens: 1000,
-// 	// });
-
-// 	const completion = await openai.createChatCompletion({
-// 		model: "gpt-3.5-turbo",
-// 		messages: [{role: "user", content: `Hello!`}],
-// 	});
-// 	console.log(completion.data.choices[0].message);
-
-// 	const basePromptOutput = completion.data.choices.pop();
-
-// 	res.status(200).json({ output: basePromptOutput });
-// };
-
-// export default generateAction;
-
-import { Configuration, OpenAIApi } from 'openai';
 const configuration = new Configuration({
-    apiKey: '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',
-    // organization: process.env.OPENAI_ORGANIZATION,
+  apiKey: process.env.OPENAI_API_KEY,
+  // organization: process.env.OPENAI_ORGANIZATION,
 });
 const openai = new OpenAIApi(configuration);
 const generateAction = async (req, res) => {
-    console.log(`API: ${basePromptPrefix}${req.body.userInput}`);
-    const completion = await openai.createChatCompletion({
-        model: 'gpt-3.5-turbo',
-        messages: [{ role: 'user', content: `${basePromptPrefix}${req.body.userInput}` }],
-        max_tokens: 2000,
-        temperature: 0,
-    });
-    console.log(completion.data.choices[0].message);
-    const basePromptOutput = completion.data.choices.pop();
-    res.status(200).json({ output: basePromptOutput });
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`);
+  const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+      { role: "user", content: `${basePromptPrefix}${req.body.userInput}` },
+    ],
+    max_tokens: 2000,
+    temperature: 0,
+  });
+  console.log(completion.data.choices[0].message);
+  const basePromptOutput = completion.data.choices.pop();
+  res.status(200).json({ output: basePromptOutput });
 };
 export default generateAction;
--- pages/index.js
+++ pages/index.js
@@ -1,119 +1,127 @@
 import Head from "next/head";
 import Image from "next/image";
-import buildspaceLogo from "../assets/buildspace-logo.png";
 import { useState } from "react";
 
 const Home = () => {
-	const [userInput, setUserInput] = useState("");
-	const [apiOutput, setApiOutput] = useState("");
-	const [isGenerating, setIsGenerating] = useState(false);
+  const [userInput, setUserInput] = useState("");
+  const [apiOutput, setApiOutput] = useState("");
+  const [isGenerating, setIsGenerating] = useState(false);
 
-	const callGenerateEndpoint = async () => {
-		setIsGenerating(true);
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true);
 
-		console.log("Calling OpenAI...");
-		const completion = await fetch("/api/generate", {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			
-			},
-			body: JSON.stringify({ userInput }),
-		});
+    console.log("Calling OpenAI...");
+    const completion = await fetch("/api/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ userInput }),
+    });
 
-		const data = await completion.json();
-		const { output } = data;
-		console.log("OpenAI replied...", output.message.content);
-		console.log(data)
+    const data = await completion.json();
+    const { output } = data;
+    console.log("OpenAI replied...", output.message.content);
+    console.log(data);
 
-		setApiOutput(`${output.message.content}`);
-		setIsGenerating(false);
-	};
+    setApiOutput(`${output.message.content}`);
+    setIsGenerating(false);
+  };
 
-	const onUserChangedText = (event) => {
-		console.log(event.target.value);
-		setUserInput(event.target.value);
-	};
+  const onUserChangedText = (event) => {
+    console.log(event.target.value);
+    setUserInput(event.target.value);
+  };
 
-	return (
-		<div className="root">
-			<Head>
-				<title>GPT-3 Writer | buildspace</title>
-			</Head>
-			<div className="container">
-				<div className="header">
-					<div className="header-title">
-						{/* Change your headline here */}
-						<h1>Get customized workout & meal plans.</h1>
-					</div>
-					<div className="header-subtitle">
-						<h2>
-							Give us your information and preferences below and we'll generate the perfect workout plan and meal plan for you. Give your information in the following format: </h2>
-							<h2>Height:</h2>
-							<h2>Weight:</h2>
-							<h2>Activity level:</h2>
-							<h2>Availability:</h2>
-							<h2>Access to equipment:</h2>
-							<h2>Diet restrictions:</h2>
-							<h2>Disliked foods:</h2>
-							<h2>Goals:</h2>
-						
-					</div>
-				</div>
+  return (
+    <div className="root">
+      <Head>
+        <title>GPT-3 Writer | buildspace</title>
+      </Head>
+      <div className="container">
+        <div className="header">
+          <div className="header-title">
+            {/* Change your headline here */}
+            <h1>Get a personalized workout plan.</h1>
+          </div>
+          <div className="header-subtitle">
+            <h2>
+              Give us your information and preferences below and we'll generate
+              the perfect workout plan for you. Give your information in the
+              following format:
+            </h2>
+            <h2>Height (in):</h2>
+            <h2>Weight (lbs):</h2>
+            <h2>
+              Goals (lose x amount of fat, gain x amount of muscle, etc.):{" "}
+            </h2>
+            <h2>Availability (days per week and minutes per day):</h2>
+            <h2>
+              Access to equipment (cardio only, free weights only, everything,
+              etc.):{" "}
+            </h2>
+            <h2>
+              Preferred form of exercise (weightlifiting, cardio, calisthenics,
+              etc.):
+            </h2>
+            {/* <h2>Height:</h2>
+            <h2>Weight:</h2>
+            <h2>Activity level:</h2>
+            <h2>Availability:</h2>
+            <h2>Access to equipment:</h2>
+            <h2>Diet restrictions:</h2>
+            <h2>Disliked foods:</h2>
+            <h2>Goals:</h2> */}
+          </div>
+        </div>
 
-				<div className="prompt-container">
-					<textarea
-						placeholder="start typing here"
-						className="prompt-box"
-						value={userInput}
-						onChange={onUserChangedText}
-					/>
-					<div className="prompt-buttons">
-						<a
-							className={
-								isGenerating ? "generate-button loading" : 
-								"generate-button"
-							}
-							onClick={callGenerateEndpoint}
-						>
-							<div className="generate">
-								{isGenerating ? (
-									<span className="loader"></span>
-								) : (
-									<p>Generate</p>
-								)}
-							</div>
-						</a>
-					</div>
-					{/* New code I added here */}
-					{apiOutput && (
-						<div className="output">
-							<div className="output-header-container">
-								<div className="output-header">
-									<h3>Output</h3>
-								</div>
-							</div>
-							<div className="output-content">
-								<p>{apiOutput}</p>
-							</div>
-						</div>
-					)}
-				</div>
-			</div>
-			<div className="badge-container grow">
-				<a
-					href="https://buildspace.so/builds/ai-writer"
-					target="_blank"
-					rel="noreferrer"
-				>
-					<div className="badge">
-						<Image src={buildspaceLogo} alt="buildspace logo" />
-						<p>build with buildspace</p>
-					</div>
-				</a>
-			</div>
-		</div>
-	);
+        <div className="prompt-container">
+          <textarea
+            placeholder={`For example:
+
+Height: 6 feet
+Weight: 180 lbs
+Goal: lose 10lbs of fat
+Availability: 5 times a week, 1 hour mins per workout
+Access to equipment: everything
+Preferred form of exercise: weightlifting`}
+            className="prompt-box"
+            value={userInput}
+            onChange={onUserChangedText}
+          />
+          <div className="prompt-buttons">
+            <a
+              className={
+                isGenerating ? "generate-button loading" : "generate-button"
+              }
+              onClick={callGenerateEndpoint}
+            >
+              <div className="generate">
+                {isGenerating ? (
+                  <span className="loader"></span>
+                ) : (
+                  <p>Generate</p>
+                )}
+              </div>
+            </a>
+          </div>
+          {/* New code I added here */}
+          {apiOutput && (
+            <div className="output">
+              <div className="output-header-container">
+                <div className="output-header">
+                  <h3>Output</h3>
+                </div>
+              </div>
+              <div className="output-content">
+                <p>{apiOutput}</p>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
